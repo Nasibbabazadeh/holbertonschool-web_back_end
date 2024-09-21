fe769c0016972a59391b8f89e65df38e724d33cb
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-21 20:15:46 +0400

    task 100 setup

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,3 @@
-export default function createEmployeesObject(departmentName, employees) {
-    return {
-      [departmentName]: employees,
-    };
+export default function createIteratorObject(report) {
+    return Object.values(report.allEmployees);
   }

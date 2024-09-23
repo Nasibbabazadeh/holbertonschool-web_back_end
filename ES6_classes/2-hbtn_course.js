00f739aa0f5819091d667a934105e0b44f70bd21
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 15:53:05 +0400

    || place fix

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,18 +1,12 @@
 export default class HolbertonCourse {
-    /* eslint disable */
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-
-    if (
-      !Array.isArray(students) ||
-      !students.every((student) => typeof student === 'string')
-    ) {
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._name = name;
@@ -47,10 +41,7 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-    if (
-      !Array.isArray(newStudents) ||
-      !newStudents.every((student) => typeof student === 'string')
-    ) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string') ) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = newStudents;

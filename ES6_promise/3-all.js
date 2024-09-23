e1fc672e31be680d62bb4dbb8423aa2ada8db627
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 16:59:05 +0400

    task 4 promise.all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function handleProfileSignup() {
+  try {
+    const results = await Promise.all([uploadPhoto(), createUser()]);
+    const [photo, user] = results;
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch {
+    console.log('Signup system offline');
+  }
+}

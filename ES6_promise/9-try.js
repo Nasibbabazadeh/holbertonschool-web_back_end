293bd1c73ba11796188fccbac718a98121cf239d
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 18:07:11 +0400

    last task try-9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,20 +1,12 @@
-import divideFunction from './8-try.js';
-
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(new Error(error.message));
+    queue.push(new Error(error));
   } finally {
     queue.push('Guardrail was processed');
   }
   return queue;
 }
-
-console.log(
-  guardrail(() => {
-    return divideFunction(0, 0);
-  })
-);

c77186f1b4abcce6d6fc620a768630f1407bba48
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 21:12:47 +0400

    pricing class task-5 eslint fix

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,10 +1,9 @@
-import Currency from './3-currency.js';
-
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
+
   get amount() {
     return this._amount;
   }

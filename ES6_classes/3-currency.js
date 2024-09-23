765e6f3abb6db3dc223921599458effb534b2e19
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 16:07:14 +0400

    task 3 currenct setup line_breaks

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -9,24 +9,29 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   set code(newCode) {
     if (typeof newCode !== 'string') {
       throw new TypeError('New code must be a string');
     }
     this._code = newCode;
   }
+
   get name() {
     return this._name;
   }
+
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('New name must be a string');
     }
     this._name = newName;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }

3540be2b0309f15fc911e5214f3c82cf04e3e84a
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 17:27:54 +0400

    final user task 7

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,5 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
-
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  });
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName)], [uploadPhoto(fileName)]);
 }

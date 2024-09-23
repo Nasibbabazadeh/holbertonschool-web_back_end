94741b2632d26519b6860df2d743e31b82cc5cf8
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 16:45:32 +0400

    task 3 then catch promise

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,6 @@
+export default function handleResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve({ status: 200, body: 'success' });
+    reject(new Error({}));
+  }).then(console.log('Got a response from the API'));
+}

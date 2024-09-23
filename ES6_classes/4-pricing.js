4ec203b742678daccc9f6090f3932a6200787e51
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 21:11:52 +0400

    pricing class task-5 class line break

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -8,18 +8,23 @@ export default class Pricing {
   get amount() {
     return this._amount;
   }
+
   set amount(newAmount) {
     this._amount = newAmount;
   }
+
   get currency() {
     return this._currency;
   }
+
   set currency(newCurrency) {
     this._currency = newCurrency;
   }
+
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency._code})`;
   }
+
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }

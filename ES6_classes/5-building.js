4c40ec467b1e23d91571d0fc8b5345d83f3d5a2c
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-23 21:46:47 +0400

    abstract method

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,13 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class Building');
+    }
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {

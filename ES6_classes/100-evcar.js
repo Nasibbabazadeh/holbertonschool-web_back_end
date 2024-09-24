eab91f3dcb38d821f7705232caf9faee502e2b88
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-24 21:25:03 +0400

    100-evcar task

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+// Import the Car class
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}

d3baff8d136533597b5a08c760e1ec62b2de40b5
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-25 18:19:52 +0400

    car class fix

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,11 +1,8 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
 class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {

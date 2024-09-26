7f849fb3ff375645da6430da1ca44cf6fc38d1a1
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-26 18:14:19 +0400

    eslint problem

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(arr) {
   const ids = arr.map((item) => item.id);
-  return ids.reduce((prev, current) => (prev += current));
+  return ids.reduce((prev, current) => prev + current, 0);
 }
